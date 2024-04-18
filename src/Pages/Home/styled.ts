29bd08982ee176d6f894da9f8fb6d88e61c5c9dc
Egor Stupenkov <109738127+ExStu@users.noreply.github.com>
2024-04-18 20:19:22 +0300

    fix: edgeBgFix

--- src/Pages/Home/styled.ts
+++ src/Pages/Home/styled.ts
@@ -1,9 +1,7 @@
 import { styled } from "@mui/material/styles";
 
-import heroEdgeBg from "@assets/images/hero-edgeBg.svg";
-
 export const SHomeHeroEdgeBg = styled("div")(() => ({
-  backgroundImage: `url(${heroEdgeBg})`,
+  backgroundImage: "url(/hero-edgeBg.svg)",
   backgroundSize: "45%",
   backgroundRepeat: "no-repeat",
   backgroundPosition: "right -85px top",
